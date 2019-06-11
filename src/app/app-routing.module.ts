8a284fe5ceb871672b937275cd3711d494d21b2b
maxsv0 <max.svistunov@gmail.com>
2019-06-11 09:38:16 +0200

    Define routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {IndexComponent} from './index/index.component';
+import {HomeComponent} from './home/home.component';
+import {AuthGuard} from './auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: IndexComponent},
+  {path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
+
+  // otherwise redirect to home
+  {path: '**', redirectTo: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
